3e6303f214db91c83294d17f40bbb23d54bafd8d
HanxiongXiao <296241332@qq.com>
2017-07-17 17:30:10 +0800

    临时提交

--- decoder.py
+++ decoder.py
@@ -1,1 +1,40 @@
-__author__ = 'admin'
+
+import tensorflow as tf
+
+class Decoder(object):
+    def __init__(self, sess, decoder, z_dim, name='Decoder'):
+        self.decoder = decoder
+        self.z_dim = z_dim
+        self.name = name
+        self.sess = sess
+
+    def init_model(self):
+        in_list = [self.z_dim]
+        in_list.extend(self.decoder[:-1])
+        out_list = self.decoder[:]
+        with tf.variable_scope(self.name) as vs:
+            for n, (in_dim, out_dim) in enumerate(zip(in_list, out_list)):
+                self._set_vars(in_dim=in_dim, out_dim=out_dim, name="layer_"+str(n))
+
+    def _set_vars(self, in_dim, out_dim, name, stddev=0.1):
+        with tf.variable_scope(name) as vs:
+            k = tf.get_variable('W', [in_dim, out_dim],
+                                initializer=tf.truncated_normal_initializer(stddev=stddev))
+            b = tf.get_variable('b', [out_dim],
+                                initializer=tf.constant_initializer(0))
+        return k, b
+
+    def feedforward(self, input):
+        in_list = self.encoder[:]
+        out_list = self.encoder[1:]
+        out_list.append(self.z_dim)
+        with tf.variable_scope(self.name, reuse=True) as vs:
+            for n, (in_dim, out_dim) in enumerate(zip(in_list, out_list)):
+                self._encoder_onestep(in_dim=in_dim, out_dim=out_dim, name="layer_"+str(n))
+
+    def _encoder_onestep(self, input, name):
+        with tf.variable_scope(name, reuse=True) as vs:
+            W = tf.get_variable('W')
+            b = tf.get_variable('b')
+            a = tf.matmul(input, W) + b
+        return tf.nn.sigmoid(a)
--- discriminator.py
+++ discriminator.py
@@ -8,16 +8,32 @@ class Discriminator(object):
         self.name = name
 
     def init_var(self):
-        in_list = [self.z_dim, self.layers[:-1]]
+        in_list = [self.z_dim]
+        in_list.extend(self.layers[:-1])
         out_list = self.layers[:]
         with tf.variable_scope(self.name):
             for n, (in_dim, out_dim) in enumerate(zip(in_list, out_list)):
-                    self._set_dis_var(in_dim=in_dim, out_dim=out_dim, name="layer_"+str(n))
+                    self._set_vars(in_dim=in_dim, out_dim=out_dim, name="layer_"+str(n))
 
-    def _set_dis_var(self, in_dim, out_dim, name, stddev=0.1):
+    def _set_vars(self, in_dim, out_dim, name, stddev=0.1):
         with tf.variable_scope(name) as vs:
             k = tf.get_variable('W', [out_dim, in_dim],
                                 initializer=tf.truncated_normal_initializer(stddev=stddev))
             b = tf.get_variable('b', [out_dim, 1],
                                 initializer=tf.constant_initializer(0))
         return k, b
+
+    def feedforward(self, input):
+        in_list = [self.z_dim]
+        in_list.extend(self.layers[:-1])
+        out_list = self.layers[:]
+        with tf.variable_scope(self.name, reuse=True) as vs:
+            for n, (in_dim, out_dim) in enumerate(zip(in_list, out_list)):
+                self._feedforward_onestep(in_dim=in_dim, out_dim=out_dim, name="layer_"+str(n))
+
+    def _feedforward_onestep(self, input, name):
+        with tf.variable_scope(name, reuse=True) as vs:
+            W = tf.get_variable('W')
+            b = tf.get_variable('b')
+            a = tf.matmul(input, W) + b
+        return tf.nn.sigmoid(a)
